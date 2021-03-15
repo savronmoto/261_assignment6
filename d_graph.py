42fd6aada003e3b7f0de20b16b105c46aa71390e
savanna <hansonsa@oregonstate.edu>
2021-03-15 14:39:56 -0700

    fixed bug on bfs

--- d_graph.py
+++ d_graph.py
@@ -212,9 +212,10 @@ class DirectedGraph:
             if v not in visited_vertices:
                 visited_vertices.append(v)
             temp = []
-            for ele in self.adj_matrix[v]:
+            for i in range(len(self.adj_matrix[v])):
+                ele = self.adj_matrix[v][i]
                 if ele != 0:
-                    temp.append(self.adj_matrix[v].index(ele))
+                    temp.append(i)
                 temp.sort(reverse=True)
                 for i in temp:
                     if i not in visited_vertices:
